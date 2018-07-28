743eed441888daed31e838ff76bb648ffffc19bc
Bumpversion after CI <bumpversion-after-ci@example.com>
2018-07-28 07:47:58 +0000

    Bump version: 0.0.9 -> 0.0.10 [ci skip]

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from os import path
 
 setup(
     name='invoicex-gui',
-    version='0.0.9',
+    version='0.0.10',
     author='Harshit Joshi',
     author_email='harshit.joshi@outlook.com',
     url='https://github.com/invoice-x/invoicex-gui',
